339ef9d85d4a457f666a2f6bf6bf0e83eaae9f44
wartech0 <wartech0@hotmail.com>
2014-11-07 01:18:46 -0600

    Added qt support, and window

---
+++ examples/visualization/mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QTimer::singleShot(1000, this, SLOT(showFullScreen()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
