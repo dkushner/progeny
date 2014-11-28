3761c9be03e5ba4617e142269bbfbaa0603f22f4
Dave Kushner <dkushner2@mail.csuchico.edu>
2014-11-28 13:51:29 -0800

    Fix roulette selection.

--- src/selectors/roulette_selector.h
+++ src/selectors/roulette_selector.h
@@ -9,55 +9,57 @@
 
 namespace pr {
 
-  template <typename CType>
-  class RouletteSelector : public Selector<CType> {
+template <typename CType>
+class RouletteSelector : public Selector<CType> {
 
-    using Candidate = CType;
-    using Population = pr::Population<Candidate>;
-    using FitnessType = typename Candidate::FitnessType;
+  using Candidate = CType;
+  using Population = pr::Population<Candidate>;
+  using FitnessType = typename Candidate::FitnessType;
 
-    public:
-      virtual void select(Population& pop, int count, bool natural = true) {
-        std::default_random_engine gen;
-        std::vector<FitnessType> weights(pop.size());
+  public:
+    virtual void select(Population& pop, int count, bool natural = true) {
 
-        // If necessary, re-normalize population.
-        if (!natural) {
-          FitnessType max_fit{};
-          #pragma omp parallel
-          {
-            #pragma omp for reduction(max : max_fit)
-            for (int x = 0; x < pop.size(); ++x) {
-              if (pr::fitness(pop[x]) > max_fit) {
-                max_fit = pr::fitness(pop[x]);
-              }
-            }
-            #pragma omp flush(max_fit)
+      std::default_random_engine gen;
+      std::vector<FitnessType> weights(pop.size());
 
-            #pragma omp for
-            for (int i = 0; i < pop.size(); ++i) {
-              weights[i] = (max_fit + 1) - pr::fitness(pop[i]);
-              pop[i].alive = false;
+      // If necessary, re-normalize population.
+      if (!natural) {
+        FitnessType max_fit{};
+        #pragma omp parallel
+        {
+          #pragma omp for reduction(max : max_fit)
+          for (int x = 0; x < pop.size(); ++x) {
+            if (pr::fitness(pop[x]) > max_fit) {
+              max_fit = pr::fitness(pop[x]);
             }
           }
-        } else {
-          #pragma omp parallel for
+          #pragma omp flush(max_fit)
+
+          #pragma omp for
           for (int i = 0; i < pop.size(); ++i) {
-            weights[i] = pr::fitness(pop[i]);
+            weights[i] = (max_fit + 1) - pr::fitness(pop[i]);
             pop[i].alive = false;
           }
         }
-
-        std::discrete_distribution<> dist(weights.begin(), weights.end());
-
-        for (int i = 0; i < count; ++i){
-          int idx = dist(gen);
-          weights[idx] = 0.0;
-          dist.param({ weights.begin(), weights.end() });
-          pop[idx].alive = true;
+      } else {
+        #pragma omp parallel for
+        for (int i = 0; i < pop.size(); ++i) {
+          weights[i] = pr::fitness(pop[i]);
+          pop[i].alive = false;
         }
       }
-  };
-}
 
+      std::discrete_distribution<> dist(weights.begin(), weights.end());
+
+      for (int i = 0; i < count; ++i){
+        int idx = dist(gen);
+        weights[idx] = 0.0;
+        dist.param({ weights.begin(), weights.end() });
+        pop[idx].alive = true;
+      }
+    }
+};
+
+
+} // ::pr
 #endif
